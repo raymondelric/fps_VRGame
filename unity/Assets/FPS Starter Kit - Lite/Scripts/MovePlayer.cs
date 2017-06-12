1e7cb4a5d85b159261beb2574539505034ec03b9
b03902046@ntu.edu.tw <bsk26923@gmail.com>
2017-06-13 02:35:09 +0800

    Push unity

---
+++ unity/Assets/FPS Starter Kit - Lite/Scripts/MovePlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class MovePlayer : MonoBehaviour {
+
+	public float playerSpeed;
+	public static SocketIOComponent socket;
+
+	// Use this for initialization
+
+	void Start () {
+
+		GameObject go = GameObject.Find("SocketIO");
+		socket = go.GetComponent<SocketIOComponent>();
+		
+		Debug.Log("Hi, start");
+
+		socket.On("setX", SetXfunc);
+		
+		StartCoroutine("BeepBoop");	
+	}
+
+	private IEnumerator BeepBoop()
+	{
+		while(true){
+
+			yield return new WaitForSeconds(0.3f);
+			socket.Emit("beep");
+		}
+	}
+
+	public void SetXfunc(SocketIOEvent e) {
+		Debug.Log("I'm at setX");
+		Debug.Log("[SocketIO] SetX received: " + e.name + " " + e.data);
+		playerSpeed = float.Parse(e.data["X"].str) / 20;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+	}
+}
